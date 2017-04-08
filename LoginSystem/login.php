c50b4abb3838d5cf8c987117c66a3e4e6f79a44b
Brian Najera <brian2najera@gmail.com>
2017-04-08 04:57:51 -0500

    prepin login backend

--- LoginSystem/login.php
+++ LoginSystem/login.php
@@ -1,64 +1,119 @@
-<!DOCTYPE html>
-<head>
-	<title>Login Page</title>
-	<link rel="stylesheet" href="common.css">
-	<link rel="stylesheet" href="login.css">
-	<link rel="stylesheet" href="/style/drop-down-menu.css">
-	<script src="/site/script/common.js"></script>
-	<script>
-		// add functionality to individual elements
-		document.addEventListener("DOMContentLoaded", function (event) {
-		});
-	</script>
-</head>
+<?php
 
+session_start();
+include_once 'connectDB.php';
+
+if(isset($_SESSION['logged_in']))
+{
+    header("Location: index.php");
+    exit;
+}
+
+$error = false;
+
+if(isset($_POST['sign_up']))
+{
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    // prevent sql injection.
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = true;
+        echo "Please Enter a Valid Email";
+    }
+
+
+
+    $sql = "SELECT * FROM Member WHERE email = '$email'";
+    $result = $con->query($sql);
+
+    if ($result->num_rows > 0) {
+        $error = true;
+        echo "email already in use";
+    }
+
+    if(!$error) // if there is no error, continue to logging in.
+    {
+
+    }
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<title>Login</title>
+
+<link rel="stylesheet" href="common.css">
+<link rel="stylesheet" href="drop-down-menu.css">
+
+<link rel="stylesheet" href="login.css">
+
+<script src="script/common.js"></script>
+<script>
+    document.addEventListener("DOMContentLoaded",
+        function (event) {
+            // make stuff happen after page loads
+        }
+    );
+</script>
 <body>
-	<header>
-		<h1><a href="http://www.databaseteam12.x10host.com"><font color="white">University of Houston</font></a></h1>
+<header>
+    <div class="main">
+        <h1><a href="http://www.databaseteam12.x10host.com"><font color="white">University of Houston</font></a></h1>
         <h3><a href="http://www.databaseteam12.x10host.com"><font color="white">Libraries</font></a></h3>
-	</header>
-	<nav>
-		
-	</nav>
-	<!--custom html below-->
-	<main>
-		<section id="sign-in">
-			<h2 style="text-align:center;">Sign in</h2>
-			<form action="processData.php" method="POST">
-				<!--action="processData" sends the data that was inputed in the form to the. POST method is used for sensetive data.-->
-				<p>
-					<label>Email:</label><br>
-					<input type="text" name="email">
-					<br>
-				</p>
-				<p>
-					<label>Password:</label><br>
-					<input type="password" name="password">
-					<a href="/page/login/forgotpass.php" style="float:right;">
-						Forgot your Password?
-					</a>
-					<br><br>
-				</p>
-				<p>
-					<button type="submit">Sign in</button>
-				</p>
-				<div class="divider">
-					<hr class="left"> <small>Need an account?</small> <hr class="right">
-				</div>
-				<div class="regbox">
-					<p>
-						<button class="registerbutton" type="submit">Create an account</button>
-					</p>
-				</div>
-			</form>
-		</section>
-	</main>
-	<!--custom html above-->
-	<footer>
-		&copy; Spring 2017 COSC 3380 Team 12
-		<br><br>
-		4333 University Drive
-		<br>
-		Houston, TX 77204-2000
-	</footer>
+    </div>
+    <div class="subhead">
+
+    </div>
+</header>
+
+<nav>
+    <a href="index.php" style="float:left;">Home</a>
+    <a href="register.php">Register</a>
+</nav>
+
+<!-- custom content -->
+<main id="login">
+    <section id="sign-in">
+        <h2 style="text-align:center;">Sign in</h2>
+        <form action="processData.php" method="POST">
+            <!--action="processData" sends the data that was inputed in the form to the. POST method is used for sensetive data.-->
+            <p>
+                <label>Email:</label><br>
+                <input type="text" name="email" required="">
+                <br>
+            </p>
+            <p>
+                <label>Password:</label><br>
+                <input type="password" name="password" required="">
+                <a href="resetpass.php" style="float:right;">
+                    Forgot your Password?
+                </a>
+                <br><br>
+            </p>
+            <p>
+                <button type="submit">Sign in</button>
+            </p>
+            <div class="divider">
+                <hr style="float:left;"> <small>Need an account?</small> <hr style="float:right;">
+            </div>
+            <div class="regbox">
+                <p>
+                    <button class="registerbutton" type="submit">Create an account</button>
+                </p>
+            </div>
+        </form>
+    </section>
+</main>
+
+<footer>
+    &copy; Spring 2017 COSC 3380 Team 12
+    <br><br>
+    4333 University Drive
+    <br>
+    Houston, TX 77204-2000
+</footer>
 </body>
+</html>
