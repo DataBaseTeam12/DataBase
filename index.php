a5b579724ee3935fde6f7a908c43aa7417071ef5
phucvu123 <pvu2608@gmail.com>
2017-04-11 16:41:42 -0500

    fix faculty

--- index.php
+++ index.php
@@ -1,149 +1,93 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<!--Page title in tab-->
-		<title>Home</title>
-		<!--Link to CSS page-->
-		<link rel="stylesheet" href="library.css">
-	</head>
-	<body>
-		<div class="container">
-			<!--Header section-->
-			<header>
-				<!--Larger header section-->
-				<div class="main">
-					<h1>University of Houston</h1>
-					<h3>Libraries</h3>
+<head>
+	<!--Page title in tab-->
+	<title>Home</title>
+	<!--Link to CSS page-->
+	<link rel="stylesheet" href="library.css">
+	<link rel="stylesheet" href="/style/drop-down-menu.css">
+</head>
+<body>
+	<div class="container">
+		<!--Header section-->
+		<header>
+			<!--Larger header section-->
+			<div class="main">
+				<h1>University of Houston</h1>
+				<h3>Libraries</h3>
+			</div>
+			<!--Bar containing Home button, Login/Logout-->
+			<div class="subhead">
+				<form action="" style="width: 62px; float: left; margin-left: 20px;">
+					<button>Home</button>
+				</form>
+				<form action="http://databaseteam12.x10host.com/login/register.php" style="width: 91px; float: right;">
+					<button>Register</button>
+				</form>
+				<form action="http://databaseteam12.x10host.com/login/login.php" style="width: 64px; float: right;">
+					<button>Login</button>
+				</form>
+			</div>
+		</header>
+		<!--Main section-->
+		<!--Navigation sidebar-->
+		<nav>
+			<div>
+			    <!--
+				<div class="nav-drop vgap">
+					Search Media
+					<div class="nav-drop-content">
+						<a href="search.php">Search</a>
+						<a href="displayAll.php">Display All Media</a>
+						<a href="displayAllBooks.php">Display All Books</a>
+						<a href="displayAllCassettes.php">Display All Cassettes</a>
+						<a href="displayAllCds.php">Display All CDs</a>
+						<a href="displayAllDvds.php">Display All DVDs</a>
+						<a href="displayAllVhs.php">Display All VHS</a>
+					</div>
 				</div>
-				<!--Bar containing Home button, Login/Logout-->
-				<div class="subhead">
-               		<form action="" style="width: 62px; float: left; margin-left: 20px;">
-						<button>Home</button>
-					</form>
-					<form action="/register.php" style="width: 91px; float: right;">
-						<button>Register</button>
-					</form>
-					<form action="/login.php" style="width: 64px; float: right;">
-						<button>Login</button>
-					</form>
+				<div class="nav-drop vgap">
+					Laptop Rentals
+					<div class="nav-drop-content">
+						<a href="displayAllLaptops.php">Display All Laptops</a>
+					</div>
 				</div>
-			</header>
-			<!--Main section-->
-			<!--Navigation sidebar-->
-			<nav>
-            	<ul>
-                	<li>
-              			<div class="nav-drop">
-                			<button class="drop">All Media</button>
-							<!--Dropdown contents-->
-                			<div class="nav-drop-content">
-                  				<a href="#">Display All</a>
-                  				<a href="#">Search By Author</a>
-                			</div>
-              			</div>
-                    </li>
-                	<li>
-              			<div class="nav-drop">
-                            <button class="drop">Books</button>
-							<!--Dropdown contents-->
-                            <div class="nav-drop-content">
-                                <a href="#">Display All</a>
-                                <a href="#">Search By Title</a>
-                                <a href="#">Search By Author</a>
-                                <a href="#">Search By Genre</a>
-                                <a href="#">Search By Publisher</a>
-                                <a href="#">Search By Year Published</a>
-                                <a href="#">Search By Audience</a>
-                                <a href="#">Search By Language</a>
-                          	</div>
-                        </div>
-                    </li>
-                	<li>
-              			<div class="nav-drop">
-							<button class="drop">Cassettes</button>
-							<!--Dropdown contents-->
-							<div class="nav-drop-content">
-								<a href="#">Display All</a>
-								<a href="#">Search By Title</a>
-								<a href="#">Search By Artist (Name)</a>
-								<a href="#">Search By Artist (Band)</a>
-								<a href="#">Search By Genre</a>
-								<a href="#">Search By Publisher</a>
-								<a href="#">Search By Year Published</a>
-								<a href="#">Search By Audience</a>
-								<a href="#">Search By Language</a>
-								<a href="#">Search By Producer</a>
-							</div>
-						</div>
-                    </li>
-                	<li>
-              			<div class="nav-drop">
-							<button class="drop">CDs</button>
-							<!--Dropdown contents-->
-							<div class="nav-drop-content">
-								<a href="#">Display All</a>
-								<a href="#">Search By Title</a>
-								<a href="#">Search By Artist (Name)</a>
-								<a href="#">Search By Artist (Band)</a>
-								<a href="#">Search By Genre</a>
-								<a href="#">Search By Publisher</a>
-								<a href="#">Search By Year Published</a>
-								<a href="#">Search By Audience</a>
-								<a href="#">Search By Language</a>
-								<a href="#">Search By Producer</a>
-							</div>
-						</div>
-                    </li>
-                	<li>
-              			<div class="nav-drop">
-							<button class="drop">DVDs</button>
-							<!--Dropdown contents-->
-							<div class="nav-drop-content">
-								<a href="#">Display All</a>
-								<a href="#">Search By Title</a>
-								<a href="#">Search By Genre</a>
-								<a href="#">Search By Director</a>
-								<a href="#">Search By Producer</a>
-								<a href="#">Search By Year Published</a>
-								<a href="#">Search By Language</a>
-							</div>
-						</div>
-                    </li>
-                	<li>
-              			<div class="nav-drop">
-							<button class="drop">VHS</button>
-							<!--Dropdown contents-->
-							<div class="nav-drop-content">
-								<a href="#">Display All</a>
-								<a href="#">Search By Title</a>
-								<a href="#">Search By Genre</a>
-								<a href="#">Search By Director</a>
-								<a href="#">Search By Producer</a>
-								<a href="#">Search By Year Published</a>
-								<a href="#">Search By Language</a>
-							</div>
-						</div>
-                    </li>
-               	</ul>
-			</nav>
-			<!--Main body-->
-			<main>
-<!--				<div class="inner">-->
-<!--					<!--Header of content
-<!--					<h2>Welcome!</h2>-->
-<!--					<!--Content-->
-<!--					<p>This is Team 12's Database project. Please login if you have an account, or register if you're new here.</p>-->
-<!--					<p>The search menu is on the left. Use it to search for media in the library's collection.</p>-->
-<!--				</div>-->
-			</main>
-			<!--Footer section-->
-			<footer>
-				&copy; Spring 2017 COSC 3380 Team 12
-				<br>
-				4333 University Drive
-				<br>
-				Houston, TX 77204-2000
-			</footer>
-		</div>
-	</body>
+				<div class="nav-drop">
+					Room Reservations
+					<div class="nav-drop-content">
+						<a href="displayAllRooms.php">Display All Rooms</a>
+					</div>
+				</div>
+				-->
+				<?php
+		        session_start();
+		        foreach ($_SESSION as $key=>$val)
+                echo $key." ".$val."<br/>";
+				if(isset($_SESSION["userAccount"]) && $_SESSION["userAccount"] == "Faculty") {
+				    include "page/menu_faculty.php";
+				    }
+		        include "page/menu-user.html";
+		        ?>
+			</div>
+		</nav>
+		<!--Main body-->
+		<main>
+			<!--				<div class="inner">-->
+			<!--					<!--Header of content
+			<!--					<h2>Welcome!</h2>-->
+			<!--					<!--Content-->
+			<!--					<p>This is Team 12's Database project. Please login if you have an account, or register if you're new here.</p>-->
+			<!--					<p>The search menu is on the left. Use it to search for media in the library's collection.</p>-->
+			<!--				</div>-->
+		</main>
+		<!--Footer section-->
+		<footer>
+			&copy; Spring 2017 COSC 3380 Team 12
+			<br>
+			4333 University Drive
+			<br>
+			Houston, TX 77204-2000
+		</footer>
+	</div>
+</body>
 </html>
