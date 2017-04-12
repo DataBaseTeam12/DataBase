11740ec51d4f99174b1bd245d1e3865dd7ef8a98
eamorgan <raucousdragon@gmail.com>
2017-04-11 19:21:25 -0500

    edited home

--- FINAL/home.php
+++ FINAL/home.php
@@ -14,13 +14,14 @@
 	<nav class="frow fill">
 		<a href="/index.php" class="fl">Home</a>
 		
-		<?php if($_SESSION['logged_in']) == false) 
+		<?php if($_SESSION['logged_in']) == false) {
 			echo "<a href='/login.php' style='margin:0px 5px;'>Login</a>
 			<a href='/register.php'>Register</a>";
-			
-			else if($_SESSION['logged_in']) == true) 
+			}
+			else if($_SESSION['logged_in']) == true) {
 			echo "<a href='/login.php' style='margin:0px 5px;'>Login</a>
 			<a href='/logout.php'>Logout</a>";
+			}
 		?>
 		
 	</nav>
@@ -31,11 +32,11 @@
 		<?php if(if (isset($_SESSION['userAccount']) && $_SESSION['userAccount'] == 'Faculty') include "new_page/menu-faculty.html"; ?>
 		<?php include "new_page/menu-user.html"; ?>
 	</aside>
-	<!--	<main class="grow-row fill-col">-->
+	<main class="grow-row fill-col">-->
 	<!--<h2> Welcome! </h2>-->
 	<!--<p> This is Team 12's Database project. Please login if you have an account, or register if you're new here. </p>-->
 	<!--<p> When logged in, the menu will display on the right. Use it to search for media in the library's collection. </p>-->
-	<!--	</main>-->
+	</main>
 </div>
 
 <?php include "new_page/common-footer.html"; ?>
